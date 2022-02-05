d9de0e1877b3c34aadc7a14ba325a9f398ea552d
Ruslan Efanov <rus.efanov@bk.ru>
2022-02-05 10:45:49 +0300

    init project

---
+++ src/Entity/Employee.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmployeeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EmployeeRepository::class)
+ */
+class Employee
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fio;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $salaryType;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFio(): ?string
+    {
+        return $this->fio;
+    }
+
+    public function setFio(string $fio): self
+    {
+        $this->fio = $fio;
+
+        return $this;
+    }
+
+    public function getSalaryType(): ?string
+    {
+        return $this->salaryType;
+    }
+
+    public function setSalaryType(string $salaryType): self
+    {
+        $this->salaryType = $salaryType;
+
+        return $this;
+    }
+}
