9b266e0531081cdb546b0e3f4afec23261897f0f
Ruslan Efanov <rus.efanov@bk.ru>
2022-05-28 16:21:38 +0300

    Установка vue

---
+++ assets/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: ['error', 4],
+    semi: 0,
+    'vue/html-indent': ['error', 4, {
+      attribute: 1,
+      baseIndent: 1,
+      closeBracket: 0,
+      alignAttributesVertically: true,
+      ignores: []
+    }],
+    yoda: [2, 'never'],
+    'space-before-function-paren': 'off'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
---
+++ assets/src/components/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h6>Страницы не существует</h6>
+  </div>
+</template>
+
+<script>
+import Vue from "vue"
+import Component from "vue-class-component"
+
+@Component
+export default class NotFound extends Vue {}
+</script>
+
+<style scoped></style>
